7ac8b1e767a4416d2ec3fbffdf708d4a679ca02f
yangyuxiao.don <yangyuxiao.don@18plan.com>
2016-07-07 09:44:45 +0800

    add iOS scheme

---
+++ Source/SwiftyPack.h
@@ -0,0 +1,13 @@
+//
+//  SwiftyPack.h
+//  SwiftyPack
+//
+//  Created by DonYang on 7/7/16. http://github.com/SwiftyPack/
+//  The MIT License
+//
+
+@import Foundation;
+
+FOUNDATION_EXPORT double SwiftyPackVersionNumber;
+
+FOUNDATION_EXPORT const unsigned char SwiftyPackVersionString[];
---
+++ Source/SwiftyPack.swift
@@ -0,0 +1,9 @@
+//
+//  SwiftyPack.swift
+//  SwiftyPack
+//
+//  Created by DonYang on 7/7/16. http://github.com/SwiftyPack/
+//  The MIT License
+//
+
+import Foundation
