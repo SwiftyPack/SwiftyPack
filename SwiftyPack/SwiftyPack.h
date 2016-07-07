c154466db278557efe028623c817e9af35551f2f
yangyuxiao.don <yangyuxiao.don@18plan.com>
2016-07-07 09:54:28 +0800

    add watchOS scheme

---
+++ SwiftyPack/SwiftyPack.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyPack.h
+//  SwiftyPack
+//
+//  Created by DonYang on 7/7/16.
+//
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for SwiftyPack.
+FOUNDATION_EXPORT double SwiftyPackVersionNumber;
+
+//! Project version string for SwiftyPack.
+FOUNDATION_EXPORT const unsigned char SwiftyPackVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyPack/PublicHeader.h>
+
+
